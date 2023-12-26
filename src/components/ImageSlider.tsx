2bfa9546e62be478ff2ef4eb44099e62abbebf94
Adam Lipert <a.lipert@protonmail.com>
2023-12-26 21:27:20 +0100

    fix: Move images and update structure

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -2,6 +2,11 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import {Pagination} from "swiper/modules";
+import Image1 from '../images/house/IMG-20230724-WA0001.jpg';
+import Image2 from '../images/house/IMG-20230724-WA0002.jpg';
+import Image3 from '../images/house/IMG-20230724-WA0003.jpg';
+import Image4 from '../images/house/IMG-20230724-WA0004.jpg';
+import Image5 from '../images/house/IMG-20230724-WA0005.jpg';
 
 export default () => {
     const pagination = {
@@ -24,19 +29,19 @@ export default () => {
             pagination={pagination}
         >
             <SwiperSlide>
-                <img src={"/images/house/IMG-20230724-WA0001.jpg"} className={"w-full object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
+                <img src={Image1.src} className={"w-full object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src={"/images/house/IMG-20230724-WA0002.jpg"} className={"w-full h-[84svh] object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
+                <img src={Image2.src} className={"w-full h-[84svh] object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src={"/images/house/IMG-20230724-WA0003.jpg"} className={"w-full h-[84svh] object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
+                <img src={Image3.src} className={"w-full h-[84svh] object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src={"/images/house/IMG-20230724-WA0004.jpg"} className={"w-full h-[84svh] object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
+                <img src={Image4.src} className={"w-full h-[84svh] object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src={"/images/house/IMG-20230724-WA0005.jpg"} className={"w-full h-[84svh] object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
+                <img src={Image5.src} className={"w-full h-[84svh] object-cover select-none"} style={{ height: 'calc(90svh - 125px)' }} alt={"Image"}/>
             </SwiperSlide>
         </Swiper>
     );
