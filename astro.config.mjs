fed8c25cc4647d401db1a4ecc057113e4547206a
Adam Lipert <a.lipert@protonmail.com>
2024-03-02 14:56:54 +0100

    Add new dependencies and update configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,30 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
-
 import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
-  site: 'https://ortieez.github.io',
-  base: '/Penzion-Kamelie',
-  integrations: [tailwind(), react(), astroI18next(), robotsTxt()]
+  site: "https://penzionkamelie.cz",
+  base: "/",
+  integrations: [
+    astroI18next(),
+    tailwind(),
+    react(),
+    robotsTxt(),
+    sitemap({
+      i18n: {
+        defaultLocale: "cs", // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en", // The `defaultLocale` value must present in `locales` keys
+          cs: "cs",
+        },
+      },
+    }),
+  ],
 });
